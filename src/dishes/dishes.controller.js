b3df9a89ba197ed4be100c4d877908e6b786e757
Gavin Costa <gavin.michael.costa@gmail.com>
2023-11-28 11:31:37 -0500

    create dishes

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -11,7 +11,62 @@ const nextId = require("../utils/nextId");
 function list(req, res, next) {
     res.json({data: dishes})
 }
+//need validatorFor(prop) check for valid name, description, price, image url
+/**-name is missing/empty
+ * -description empty/missing
+ * -price missing/ > 0 / not an integer
+ * -image_url missing/empty
+ */
+function validateInteger(req,res,next) {
+    const {price} = req.body.data
+    if (Number.isInteger(Number(price)) && Number(price) > 0) {
+        next()
+    } else {
+        next({
+            status: 400,
+            message: 'Dish must have a price that is an integer greater than 0'
+        })
+    }
+}
+
+function validatorFor(prop, propMessage) {
+    return function (req,res,next) {
+        // console.log(prop)
+        if (req.body.data[prop]) {
+            next()
+        } else {
+            // console.log(propMessage, '********************')
+            next({
+                status: 400,
+                message: `${propMessage}`
+            })
+        }
+    }
+}
+
+
+function create(req, res, next) {
+    const {data: {id, name, description, price, image_url} = {}} = req.body
+    const newDish = {
+        id: nextId(),
+        name,
+        description,
+        price,
+        image_url,
+    }
+    console.log(newDish)
+    dishes.push(newDish)
+    res.status(201).json({data: newDish})
+}
 
 module.exports = {
     list,
+    create: [
+        validatorFor('name', 'Dish must include a name'),
+        validatorFor('description', 'Dish must include a description'),
+        validatorFor('price', 'Dish must include a price'),
+        validatorFor('image_url', 'Dish must include a image_url'),
+        validateInteger, 
+        create
+        ],
 }
