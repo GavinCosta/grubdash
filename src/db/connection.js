fce39bb901db5f3b721595c5103ec931bc6b17dd
Gavin Costa <gavin.michael.costa@gmail.com>
2023-12-09 12:09:35 -0500

    updated env

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,4 +1,4 @@
-const env = pprocess.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV || "development";
 const config = require("../../knexfile")[env];
 const knex = require("knex")(config);
 
